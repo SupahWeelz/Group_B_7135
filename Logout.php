09d6ee1fb35f3beac3bdf0d15d0ba83adf2bdbfb
SupahWeelz <60757434+SupahWeelz@users.noreply.github.com>
2020-04-15 13:02:00 -0500

    Add files via upload

--- Logout.php
+++ Logout.php
@@ -4,7 +4,12 @@
 <?php
 // Initialize the session
 session_start();
- 
+if(isset($_SESSION['username'])){
+	$fp = fopen("log.html", 'a');
+	fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['username'] ." has logged out.</i><br></div>");
+	fclose($fp);
+}
+
 // Unset all of the session variables
 $_SESSION = array();
 // Destroy the session.
