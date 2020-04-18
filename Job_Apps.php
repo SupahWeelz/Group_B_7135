0657ed728599d4485b5721d92f5fce8e4a384a24
mkhatu1 <57546851+mkhatu1@users.noreply.github.com>
2020-04-17 21:49:47 -0500

    small change in style

--- Job_Apps.php
+++ Job_Apps.php
@@ -12,25 +12,27 @@
  <?php
 	// Selects and displays applications that current user has applied to
 	//$getApplications = "SELECT * FROM application, job, jobseeker WHERE jobseeker.JobSeekerID = application.JobseekerID AND A.JobID = B.JobID GROUP BY A.ApplicationID ORDER BY Close_date";
-	
+
 	$getApplications = "SELECT job.JobName,application.Applied_date,job.JobDescription,company.CompanyName FROM company,application,jobseeker,job WHERE jobseeker.JobSeekerID = application.JobseekerID AND application.JobID = job.JobID AND company.CompanyID = job.CompanyID AND jobseeker.UserID = ".$_SESSION["id"];
-	
+
 	$result = query($getApplications,$db);
 	$count = mysqli_num_rows($result);
 	//echo $count;
 	//print_r($row);
 		if ($count > 0) {
 			$myCount=1;
-			echo "<table><tr><th>#</th><th>Job Title</th><th>Submission Date</th><th>Job Description</th><th>Company</th></tr>";
+			echo "<table><tr><th>No.</th><th>Job Title</th><th>Submission Date</th><th>Job Description</th><th>Company</th></tr><hr/>";
 			while($row = mysqli_fetch_assoc($result)){
 				echo "<tr><td>".$myCount."</td><td>".$row["JobName"]."</td><td>".$row["Applied_date"]."</td><td>".$row["JobDescription"]."</td><td> ".$row["CompanyName"]."</td></tr>";
+
 				$myCount=$myCount+1;
 				if($myCount>51){
 					break;
 				}
 			}
 			echo "</table>";
-		} 
+			echo "<hr/>";
+		}
 		else {
 			echo "0 results";
 		}
