815bc06a87a86b026dc47dfd2a3312e6f9b9c810
mkhatu1 <57546851+mkhatu1@users.noreply.github.com>
2020-04-17 21:07:31 -0500

    included in header

---
+++ About.php
@@ -0,0 +1,31 @@
+<?php
+	include("include/Header.php");
+	include "include/Config.php";
+?>
+
+
+<!contact page codes goes here.........................................................................>
+
+<div align="center">
+<h1>About Us</h1>
+<hr/>
+<!------------------------------------>
+<blockquote> DCDR.sa is a renowned oil an gas association formed by 100 companies distributed over the state of Texas, USA. We help people to find their dream jobs. Search and find yours today!</blockquote>
+<h2>Contact</h2>
+------------------------------------
+<!_____________________________________>
+<p>DCDR.sa</p>
+<p>1234 Fannin St</p>
+<p>Houston, TX 71829</p>
+<p>Phone: 346-900-700</p>
+<p>Fax: 346-900-700</p>
+
+<p>Email Us: <a href=#> job@dcdr.com</a></p>
+
+
+</div>
+
+
+<?php
+	include("include/Footer.php");
+?>
